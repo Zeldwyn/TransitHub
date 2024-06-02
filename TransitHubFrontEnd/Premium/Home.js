1480b9e1bf7cd4333d1ac21ac125bf06ed6440df
Zeldwyn <143701358+Zeldwyn@users.noreply.github.com>
2024-06-02 22:57:03 +0800

    Update Home.js

--- TransitHubFrontEnd/Premium/Home.js
+++ TransitHubFrontEnd/Premium/Home.js
@@ -72,36 +72,6 @@ export default function Home() {
             });  setAccepted(false);          
     }}])};
                 
-        Alert.alert(
-            "Confirmation", "Are you sure you want to accept?",
-            [
-                { text: "Cancel", style: 'destructive' },
-                {
-                    text: "Accept", onPress: () => {
-                        fetch(`http://192.168.1.5:8080/accept-Invites`, {
-                            method: 'PUT',
-                            headers: {
-                                Accept: 'application/json',
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({
-                                'ownerID': ownerID,
-                                'premiumUserID': parseInt(pID),
-                            })
-                        })
-                        .then(response => response.json())
-                        .then(data => {
-                            console.log(data);
-                        })
-                        .catch(error => {
-                            console.error('Error posting data to Express backend:', error);
-                        });
-                    }
-                }
-            ]
-        );
-    };
-
     const renderInvite = ({ item }) => (
         <View style={styles.itemContainer}>
             <View style={styles.textContainer}>
